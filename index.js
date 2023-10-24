6313ecd9bc938d927e0bad07ea4309791fa8e4cd
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-10-24 18:40:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
 /* Your Code Here */
-
+function createEmployeeRecord(data) {
+ 
+    return {
+      firstName: data[0],
+      familyName: data[1],
+      title: data[2],
+      payPerHour: data[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  function createEmployeeRecords(data) {
+  
+    return data.map(createEmployeeRecord);
+  }
+  
+  function createTimeInEvent(dateTime) {
+  
+    const [date, hour] = dateTime.split(' ');
+    this.timeInEvents.push({ type: 'TimeIn', date, hour: parseInt(hour, 10) });
+    return this;
+  }
+  
+  function createTimeOutEvent(dateTime) {
+  
+    const [date, hour] = dateTime.split(' ');
+    this.timeOutEvents.push({ type: 'TimeOut', date, hour: parseInt(hour, 10) });
+    return this;
+  }
+  
+  function hoursWorkedOnDate(date) {
+    
+    const timeIn = this.timeInEvents.find(event => event.date === date);
+    const timeOut = this.timeOutEvents.find(event => event.date === date);
+    if (timeIn && timeOut) {
+      return (timeOut.hour - timeIn.hour) / 100;
+    }
+    return 0;
+  }
+  
+  function wagesEarnedOnDate(date) {
+    
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour;
+  }
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,4 +65,8 @@ const allWagesFor = function () {
 
     return payable
 }
+function calculatePayroll(employees) {
+    return employees.reduce((totalPayroll, employee) => totalPayroll + allWagesFor.call(employee), 0);
+  }
+  
 
